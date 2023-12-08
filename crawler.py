16895b3971e1f0f078b10bfa39c495705f253f15
ArashST79 <arash_tavan@yahoo.com>
2023-12-08 16:38:13 +0330

    preprocessing + Vader sentiment analyzer + simple portfolio management + average index

---
+++ crawler.py
@@ -0,0 +1,7 @@
+class TelegramCrawler:
+    def __init__(self, channels):
+        self.channels = channels
+    
+    def crawl_data(self):
+        # Code to crawl data from Telegram channels
+        pass
